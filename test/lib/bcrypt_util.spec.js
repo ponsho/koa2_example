1641f4f3df32d75348f5d7b58dc2a0bf2a395b35
Alfonso Ayala <ponsho@gmail.com>
2016-08-11 17:03:45 -0500

    Refactor bcrypt util tests

--- test/lib/bcrypt_util.spec.js
+++ test/lib/bcrypt_util.spec.js
@@ -1,9 +1,19 @@
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
 import * as bcrypt_utils from '../../src/lib/bcrypt_utils';
 import * as bcrypt from 'bcrypt-as-promised';
 
+require('sinon-as-promised');
 
-describe('Hash with Salt', () => {
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+describe('Bcrypt utils', () => {
   let sandbox;
+
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
   });
@@ -12,28 +22,37 @@ describe('Hash with Salt', () => {
     sandbox.restore();
   });
 
-  it('should return a string', async function() {
-    const hashedPw = await bcrypt_utils.hashWithSalt('mypw');
-    expect(hashedPw).to.be.a('string');
+  describe('Hash with Salt', () => {
+
+    it('should return a string', async function() {
+      const hashedPw = await bcrypt_utils.hashWithSalt('mypw');
+      expect(hashedPw).to.be.a('string');
+    });
+
+    it('should rethrow any error happening in the bcrypt hash function', function() {
+      sandbox.stub(bcrypt.default, 'hash').rejects('fail');
+      return expect(bcrypt_utils.hashWithSalt('wtf')).to.rejectedWith(Error);
+    });
   });
 
-  it('throw an error ', function() {
-    sandbox.stub(bcrypt.default, 'hash').rejects('fail');
-    return expect(bcrypt_utils.hashWithSalt('wtf')).to.rejectedWith(Error);
-  });
-});
+  describe('Compare', () => {
+    it('should return true for a valid password', async function() {
+      const pw = 'mypassword';
+      const hashedPw = await bcrypt_utils.hashWithSalt(pw);
+      const result = await bcrypt_utils.compare(pw,hashedPw);
+      expect(result).to.be.true;
+    });
 
-describe('Compare', () => {
-  it('should return true for a valid password', async function() {
-    const pw = 'mypassword';
-    const hashedPw = await bcrypt_utils.hashWithSalt(pw);
-    const result = await bcrypt_utils.compare(pw,hashedPw);
-    expect(result).to.be.true;
+    it('should return false for an invalid password', async function() {
+      const hashedPw = await bcrypt_utils.hashWithSalt('myRealPassword');
+      const result = await bcrypt_utils.compare('notMyPassword',hashedPw);
+      expect(result).to.be.false;
+    });
+
+    it('should rethrow any error happening in the bcrypt compare function ', function() {
+      sandbox.stub(bcrypt.default, 'compare').rejects('fail');
+      return expect(bcrypt_utils.compare('a','b')).to.rejectedWith(Error);
+    });
   });
 
-  it('should return false for an invalid password', async function() {
-    const hashedPw = await bcrypt_utils.hashWithSalt('myRealPassword');
-    const result = await bcrypt_utils.compare('notMyPassword',hashedPw);
-    expect(result).to.be.false;
-  });
 });
